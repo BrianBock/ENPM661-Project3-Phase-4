4e5a55496f1b054ba149ae2b690179464282e1ac
Brian Bock <bbock@terpmail.umd.edu>
2020-04-09 09:20:39 -0400

    archived old attempts

---
+++ old/vrep_python/turtlemove.lua
@@ -0,0 +1,9 @@
+    function myFunctionName(inInts,inFloats,inStrings,inBuffer)
+    -- inInts, inFloats and inStrings are tables
+    -- inBuffer is a string
+    
+    -- Perform any type of operation here.
+
+    -- Always return 3 tables and a string, e.g.:
+    return {},{},{},''
+end
